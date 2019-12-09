ffbc50e048114bc18c160b8c9db81b91243ab986
hannahrstannard <hannahrstannard@gmail.com>
2019-12-09 18:34:11 -0500

    Remove footer and hide news if undefined page

--- src/App.vue
+++ src/App.vue
@@ -3,28 +3,33 @@
     <app-header />
     <main>
       <router-view name="branding"></router-view>
-      <news-alerts type="info">
+      <news-alerts
+        v-if="shouldShowNewsAlert"
+        type="info"
+      >
         <template slot="news">
           <strong>Check back</strong> for more information about our upcoming programming competition for <strong>2020</strong>!
         </template>
       </news-alerts>
       <router-view/>
     </main>
-    <app-footer />
   </div>
 </template>
 <script>
 import { FadeTransition } from "vue2-transitions";
 import NewsAlerts from './components/NewsAlerts';
 import AppHeader from './layout/AppHeader';
-import AppFooter from './layout/AppFooter';
 
 export default {
   components: {
     AppHeader,
-    AppFooter,
     FadeTransition,
     NewsAlerts
+  },
+  computed: {
+    shouldShowNewsAlert: function() {
+      return this.$router.currentRoute.name !== undefined;
+    }
   }
 };
 </script>
