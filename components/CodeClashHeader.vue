7d701a8bcf82ad1a62a8b4862250c8be999673b5
hannahrstannard <hannahrstannard@gmail.com>
2020-01-11 20:52:50 -0500

    Create header component.

---
+++ components/CodeClashHeader.vue
@@ -0,0 +1,38 @@
+<template>
+  <nav
+    class="navbar header has-shadow is-primary"
+    role="navigation"
+    aria-label="main navigation"
+  >
+    <div class="navbar-brand">
+      <a
+        class="navbar-item"
+        href="/"
+      >
+        <img
+          src="./../assets/white_logo_transparent_background.png"
+          class="navbar-item__image"
+          alt="CCC"
+        >
+      </a>
+
+      <div class="navbar-burger">
+        <span />
+        <span />
+        <span />
+      </div>
+    </div>
+  </nav>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+img.navbar-item__image {
+  max-height: 7rem;
+}
+</style>
