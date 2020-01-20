8758242ce0f8f35401e44ea73408bf7f7f2c6b48
hannahrstannard <hannahrstannard@gmail.com>
2020-01-19 19:51:15 -0500

    Add previous challenge data.

---
+++ data/challenges.js
@@ -0,0 +1,35 @@
+export default [
+  {
+    2019: {
+      challenges: [
+        {
+          name: "Alpha Math",
+          description: "Either some or all of the math statement has been replaced by another character. Given one of these \"replaced\" math statements, find a combination of numbers (and symbols) to replace each character from the input set which solves the math problem.",
+          fileBeginner: "B-AlphaMath.pdf",
+          fileNonBeginner: "NB-AlphaMath.pdf"
+        },
+        {
+          name: "Grid Golf",
+          description: "Imagine a grid laid out on a wall in front of you, with each grid location having a number written on it. Putting your finger on the grid, you can move in any direction the number of spaces written on the location beneath your finger - as long as it doesn't send you off the edge (or to an invalid position)! One space that does not have a number written on it, but a letter \"e\" - this is the \"end\" location, which is the goal of your movement around the grid.",
+          fileBeginner: "B-GridGolf.pdf",
+          fileNonBeginner: "NB-GridGolf.pdf"
+        },
+        {
+          name: "World Cup Fever",
+          description: "The US women’s national team recently won the 2019 Women’s World Cup. Other nations around the world have decided to work on their passing skills to improve their chances in the next World Cup. One particular country has decided to develop a computer models to simulate efficient passing among teammates in different game scenarios, and you have been chosen to develop the algorithm for this model.",
+          fileBeginner: "B-WorldCupFever.pdf",
+          fileNonBeginner: "NB-WorldCupFever.pdf"
+        }
+      ],
+      beginner: {
+        teamName: "Callback Cats",
+        teamMembers: "Anthony Lam & Hunter Palcich",
+      },
+      nonBeginner: {
+        teamName: "Multiprocessing Moguls",
+        teamMembers: "David Carraher & Leo Guinan"
+      },
+      tagline: 'Congratulations to the winners of CCC\'s first programming competition! They demonstrated quick-thinking and amazing problem solving skills. 👏'
+    }
+  }
+]
